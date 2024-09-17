20a48d4660b4f3225c50e981e98e15034b84a1f2
Ruan Gomes <00001088857140sp@al.educacao.sp.gov.br>
2024-09-16 23:05:41 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const skip = document.querySelectorAll('.btn-proximo');
+
+skip.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
